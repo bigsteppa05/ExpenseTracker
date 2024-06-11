76608a90115e035c1a838e8fac8eb99089fe21fa
bigsteppa05 <habibforderiv05@gmail.com>
2024-06-11 11:31:15 +0300

    initializing transactions table

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -2,3 +2,15 @@ import sqlite3
 
 CONN = sqlite3.connect('company.db')
 CURSOR = CONN.cursor()
+
+def initalize_db():
+    CURSOR.execute('''
+    CREATE TABLE IF  NOR EXISTS Transactions (
+        transaction_id INTEGER PRIMARY KEY,
+        amount REAL NOT NULL,
+        date TEXT NOT NULL,
+        category_id INTEGER,
+        description TEXT,
+        FOREIGN KEY (category_id) REFERENCES Categories(category_id)                    
+    )             
+        ''' )
